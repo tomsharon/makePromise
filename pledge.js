c266af08787b57d75c01bce1f4ea76c57a37e95c
Tom Sharon <tomsharo9591@gmail.com>
2015-11-19 15:35:34 -0500

    End of workshop

--- pledge.js
+++ pledge.js
@@ -3,9 +3,92 @@ Promises Workshop: build the pledge.js deferral-style promise library
 ----------------------------------------------------------------*/
 // YOUR CODE HERE:
 
+var $Promise = function() {
+	this.state = "pending";
+	this.handlerGroups = [];
+
+}
+
+$Promise.prototype.then = function(success, error) {
+	if(typeof success !== 'function') {
+		success = null;
+	}
+	if(typeof error !== 'function') {
+		error = null;
+	}
+	var handlers = {
+			successCb: success,
+			errorCb: error
+	}
+	this.handlerGroups.push(handlers);
+
+	// // this.handlerGroups[0].successCb();
+	// // this.handlers.successCb()
+	if(success){
+		// if(this.state === "resolved"){
+		// // this.handlerGroups[0].successCb.call(null, this.value);
+
+		// for(var x=0;x<this.handlerGroups.length;x++)
+		// {
+		// 	this.handlerGroups[x].successCb.call(null,this.value);
+		// }
 
 
+		// this.handlerGroups.forEach(function(handlergrp){
+		// 	handlergrp.successCb.call(null, this.value);
+		// })
+		if(this.state === 'resolved'){
+			this.handlerGroups[0].successCb.call(null,this.value);
+			this.handlerGroups.pop();
 
+		}
+	}
+}
+
+
+var Deferral = function() {
+	this.$promise = new $Promise();
+	// this.switchResolve = false;
+	// this.switchReject = false;
+}
+
+var defer = function() {
+	return new Deferral();
+}
+
+Deferral.prototype.resolve = function(dataObj) {
+	
+	// if(this.switchResolve === false) {
+	// 	this.switchResolve = true;
+	// 	this.$promise.value = dataObj;
+	// }
+	if(this.$promise.state === 'pending') {
+		this.$promise.value = dataObj;
+		if(this.$promise.handlerGroups){
+			for(var x = 0; x < this.$promise.handlerGroups.length; x++) {
+				this.$promise.value = this.$promise.handlerGroups[x].successCb();
+			}
+		}
+		this.$promise.state = 'resolved';
+	}
+	return this.$promise.value;
+
+}
+
+Deferral.prototype.reject = function(dataObj) {
+	
+	// if(dataObj && !this.$promise.value) this.$promise.value = dataObj;
+
+	if(this.$promise.state === 'pending') {
+		this.$promise.value = dataObj;
+		this.$promise.state = 'rejected';
+	}
+}
+
+
+// $Promise.prototype.callHandlers = function() {
+
+// }
 
 
 /*-------------------------------------------------------
